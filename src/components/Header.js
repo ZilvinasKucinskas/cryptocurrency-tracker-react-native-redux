df129aacbb49768cee96a2ff874c9d9397d0199d
ZilKucinskas <zil.kucinskas@gmail.com>
2017-12-26 19:50:36 +0200

    Implement App Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Header = () => {
+  return (
+    <View>
+      <Text>Cryptocurrency Tracker App</Text>
+    </View>
+  )
+}
+
+export default Header;
